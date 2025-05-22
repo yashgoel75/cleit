8e9e252712ac27875e183d15f5c845505e9b35a0
yashgoel <yash.goel8370@gmail.com>
2025-05-22 23:21:10 +0530

    commit

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -7,18 +7,19 @@ function Dashboard() {
       <div className="flex flex-col items-center mt-10">
         <Image src={logo} alt="Cleit" width={200} />
       </div>
-      <div className="flex flex-col items-center w-5/6 h-3/5 m-auto border-1 border-gray-300 rounded-lg p-6 mt-20">
+      <div className="flex flex-col items-center min-w-120 w-3/6 h-3/5 m-auto border-1 border-gray-300 rounded-lg p-6 mt-20">
         <div className="text-2xl font-bold mb-4">
           Convert from ETH to any other token/currency
         </div>
-        <div className="flex flex-col">ETH</div>
-        <input className="w-97/100 border-1 border-gray-400 rounded-md p-1"></input>
+        <div className="flex flex-row justify-start w-97/100">ETH</div>
+        <input className="w-97/100 border-1 border-gray-400 rounded-md pt-1 pb-1 pl-3 pr-3"></input>
         <br></br>
-        <div className="flex flex-col">ETH</div>
-        <input type="option" className="w-97/100 border-1 border-gray-400 rounded-md p-1"></input>
-        <div className="convert-button">
-        Convert
-      </div>
+        <div className="flex flex-row justify-start w-97/100">ETH</div>
+        <input
+          type="option"
+          className="w-97/100 border-1 border-gray-400 rounded-md pt-1 pb-1 pl-3 pr-3"
+        ></input>
+        <div className="convert-button">Convert</div>
       </div>
     </div>
   );
