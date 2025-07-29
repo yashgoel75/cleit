91a5093f3c41e6cd6bb6a50900759b50aab58c0f
Naman Arora <namanarora1022@gmail.com>
2025-07-29 14:15:45 +0530

    feat: add firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,43 @@
-import logo from "../../public/cleitLogo.png";
-import Image from "next/image";
-import Header from "./Header/page";
+'use client';
+import Header from './Header/page';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../config/firebase';
+
+const provider = new GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
 export default function Home() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+    const handleClick = async () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential =
+                    GoogleAuthProvider.credentialFromResult(result);
+                const token = credential!.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+
+                console.log(user);
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential =
+                    GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    };
+
+    return (
+        <>
+            <Header />
+            <button onClick={() => handleClick()}>Sign In</button>
+        </>
+    );
 }
