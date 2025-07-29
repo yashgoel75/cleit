91785b1cd6487bbebdde6039f9ad7de936d8c12f
yashgoel <yash.goel8370@gmail.com>
2025-07-29 15:26:43 +0530

    Frontend for Student's Registration

--- src/app/Register/Society/page.tsx
+++ src/app/Register/Society/page.tsx
@@ -4,7 +4,14 @@ export default function Society() {
   return (
     <>
       <div className="w-98/100 md:w-1/2 m-auto">
-        <div className="border-1 px-3 border-gray-400 rounded-md">Society</div>
+        <div className="border-1 px-3 border-gray-400 rounded-md">
+          <div className="flex justify-center">Society</div>
+          <div className="py-5">
+            <form>
+              <label>Society Name</label>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
