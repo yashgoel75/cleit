a89bc57e070fa7187e5411b5dbcfb5f7d2f51aaf
yashgoel <yash.goel8370@gmail.com>
2025-07-31 15:18:46 +0530

    feat: input verification

--- src/app/auth/Register/Member/page.tsx
+++ src/app/auth/Register/Member/page.tsx
@@ -6,6 +6,7 @@ import "./page.css";
 import Tooltip from "@/app/Tooltip/page";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../../lib/firebase";
+import Link from "next/link";
 
 export default function Member() {
   const router = useRouter();
@@ -52,6 +53,7 @@ export default function Member() {
   const [isNameEmpty, setIsNameEmpty] = useState(false);
   const [isUsernameEmpty, setIsUsernameEmpty] = useState(false);
   const [isEmailEmpty, setIsEmailEmpty] = useState(false);
+  const [isOtpVerificationFailed, setIsOtpVerificationFailed] = useState(false);
   const [isPasswordEmpty, setIsPasswordEmpty] = useState(false);
   const [isConfirmPasswordEmpty, setIsConfirmPasswordEmpty] = useState(false);
   const [isStartYearEmpty, setIsStartYearEmpty] = useState(false);
@@ -115,6 +117,10 @@ export default function Member() {
       setIsStartYearEmpty(formData.startYear == "");
       setIsEndYearEmpty(formData.endYear == "");
       setIsDepartmentEmpty(formData.department == "");
+      return;
+    }
+    if (invalidOtp || !validOtp) {
+      setIsOtpVerificationFailed(true);
       return;
     }
     setIsSubmitting(true);
@@ -475,7 +481,22 @@ export default function Member() {
                 Verify
               </button>
             </div>
+            {isOtpVerificationFailed ? (
+              <div className="text-sm flex text-[#8C1A10] mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="18px"
+                  viewBox="0 -960 960 960"
+                  width="18px"
+                  fill="#8C1A10"
+                >
+                  <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                </svg>
+                &nbsp; Please verify your Email ID
+              </div>
+            ) : null}
           </div>
+
           {invalidOtp ? (
             <div className="flex text-sm md:text-base justify-center md:items-center bg-red-300 text-red-800 rounded px-3 text-center py-1">
               <svg
@@ -800,6 +821,12 @@ export default function Member() {
             </button>
           </div>
         </form>
+      </div>
+      <div className="text-center mb-8">
+        Already have an account?&nbsp;
+        <Link href={"/auth/Login"}>
+          <u>Login now.</u>
+        </Link>
       </div>
     </div>
   );
